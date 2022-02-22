d931ab5573b6619eddf6538dc8a4126cb7789008
martin carrizo <91659714+Greyastratos@users.noreply.github.com>
2022-02-22 01:51:46 -0300

    First Comit

---
+++ primerclaseback.js
@@ -0,0 +1,57 @@
+/* Primera entrega Back-end */
+/* 1 Declarar una funcion constructora  llamada Usuario que reciba nombre
+libros(array de objetos ) y mascotas (array de strings)
+
+getFullName: Debe retomar un string con el nombre y apellido del usuario (utilizar tamplate string)
+
+addMascota: Debe agregar una mascota (mascota) al arreglo de mascotas del usuario no debe retornar nada
+
+getMascotas: debe retomar la cantidad de mascotas que tiene el usuario.
+
+*/
+
+const usuario=(nombre,apellido)=>{
+[nombre,apellido]
+}
+
+usuario('martin', 'Carrizo');
+
+
+console.log(usuario);
+
+
+
+/*
+Consigna 
+addBook: Recibe un string book y un string autor debe agregar un objeto { nombre: book, autor: autor} al arrgelo
+de libros del usuario. No debe retornar nada.
+
+getBooks: De retornar un arreglo con solo los nombres del arreglo de libros del usuario
+
+Crear un objeto llamado usuario a partir de la funcion constructora, con valores que 
+permitan probar sus metodos. Hacer el test completo empleado la consola del navegador 
+
+2) Realizar todo lo anterior utizado el contructor de ES6 
+
+Aspectos a incluir en el entregable 
+
+Se debe proporcionar el codigo completo dentro de un script interno interno en dos decumentos
+HTML: uno para la resolucion con la funcion constructora y el otro con class.
+
+getMascotas: Suponiendo que el usuario tiene estas mascotas ['perro','gato'] usuario.getMacotas() 
+deberia devolver 2.
+
+getBooks: Suponiendo que el usuario tiene estos libros: [{nombre:'El señor de las moscas',
+ autor:'Wiliam Golding',{nombre: 'Fundacion', autor:'Isacc Asimoc'}}]
+
+ getFullName: Suponiendo que el usuario tiene: nombre: 'Elon' y apellido:'Musk' usuario.getFullName
+ deberia devolver 'Elon Musk'
+
+*/
+
+
+function constructor (){
+
+}
+/* */
+
