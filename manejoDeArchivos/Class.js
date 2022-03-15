1c648476a297e4476066d8a45c177c2fe64b157b
martin carrizo <91659714+Greyastratos@users.noreply.github.com>
2022-03-14 22:27:22 -0300

    entrega manejo de archivos

---
+++ manejoDeArchivos/Class.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+
+class Archivo  {
+    constructor(nombreAarchivo, title, price, tumbnail){
+        this.nombreAarchivo = nombreAarchivo; 
+        this.title = title;
+        this.price = price;
+        this.tumbnail= tumbnail;
+    }
+
+     leer  = async ()=>{
+               try {
+                    let contenido = await fs.promises.readFile(this.nombreAarchivo,'utf-8')
+                    console.log(contenido)
+                }
+             catch(err){
+                 console.log(`no se pudo leer` + err)
+                }
+            }
+
+     borrar= ()=>{
+         fs.promises.unlink(this.nombreAarchivo).catch(error => console.log('nope'));
+     }
+
+     guardar= async(_title, _price, _tumbnail) =>{
+         let Objnew = {
+             "title":this.title,
+             "price":this.price,
+             "tumbnail":this.tumbnail
+         }     
+         try{
+            let data = await fs.promises.readFile(this.nombreAarchivo,'utf-8')
+            let contenido = JSON.parse(data)
+            Objnew = JSON.stringify(Objnew);
+            contenido.push(Objnew);
+            console.log(JSON.stringify(contenido))
+        await fs.promises.appendFile(this.nombreAarchivo,JSON.stringify(contenido))
+         }
+         catch(err){
+             console.log('nope' +err )
+         }
+        }
+}
+
+const pepe = new Archivo('prueba.txt','hola',129,'hola');
+
+pepe.guardar();
